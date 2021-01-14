fd5104e8c03f8a34379e6fba5de7e25edce4aee0
Rocco <sadarocco@gmail.com>
2021-01-13 22:01:44 -0300

    first commit

---
+++ threads.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+
+/**
+ * Try out the thread pool of node
+ * By default nodes creates 4 threads
+ * So the first 4 operations should take more or less the same amount of time
+ * But the 5 operation is going to be waiting until 1 thread gets free
+ */
+const start = Date.now();
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('1:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('2:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('3:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('4:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('5:', Date.now() - start);
+});
