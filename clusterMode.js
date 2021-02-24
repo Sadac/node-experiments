e498f568a9a8ffb6255190f074c5f7c6b699bd0d
Rocco <sadarocco@gmail.com>
2021-02-23 22:56:25 -0300

    update

--- clusterMode.js
+++ clusterMode.js
@@ -1,4 +1,7 @@
 /* eslint-disable */
+// set to 1 the thread pool size of EVERY child cluster and main master
+// So for experiment purpose we only will have 1 thread per instance forked
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 console.log(cluster.isMaster);
@@ -11,28 +14,25 @@ if (cluster.isMaster) {
   // So we can have 2 Even Loops
   cluster.fork();
   cluster.fork();
-  cluster.fork();
+  // cluster.fork();
 } else {
-  //
+  const crypto = require('crypto');
   const express = require('express');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
 
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there dude!');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there dude!');
+    });
   });
 
   // If we call the 2 endpoints in parallel, with the fork()
   // the /fast will be executed immediately, but without the fork()
   // the /fast endpoint will wait until / gets resolved
-  app.get('/fast', (req, res)=> {
+  app.get('/fast', (req, res) => {
     res.send('This was fast...!');
-  })
+  });
 
   app.listen(3000, () => console.log('listening on port 3000'));
 }
